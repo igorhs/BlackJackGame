740840219765263c0c07d7599f22d50e5fc7a769
Igor Henrique <igor_henrique8@hotmail.com>
2016-03-04 22:30:58 -0300

    Update: Commentaries

--- src/finalproject_igorsilva/FinalProject_IgorSilva.java
+++ src/finalproject_igorsilva/FinalProject_IgorSilva.java
@@ -3,6 +3,9 @@ package finalproject_igorsilva;
 /**
  * @author Igor Henrique da Silva
  */
+ 
+ //Criando classe para exibir a tela(ambiente) do jogo.
+ //Creating a class to show the game frame.
 public class FinalProject_IgorSilva {
         public static void main(String[] args) {
         BlackJack_Frame f = new BlackJack_Frame();
